7f398726629135a7f7de16e3f5efaa45212fc3c3
Smiley98 <smileysofficialcinema@gmail.com>
2024-02-07 10:42:40 -0500

    Added input validation

--- Assets/Scripts/Test1.cs
+++ Assets/Scripts/Test1.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Test1 : MonoBehaviour
 {
-    [SerializeField] float sales = 500.0f;
+    // Input validation option 1 -- use the Range(min, max) annotation
+    [SerializeField] /*[Range(0.0f, 10000.0f)]*/ float sales;
 
     void Start()
     {
+        // Input validation option 2 -- apply absolute value to sales (make it positive)
+        //sales = Mathf.Abs(sales);
+
+        // Given what you had learned, you could also just multiply by negative 1
+        if (sales < 0.0f)
+            sales = sales * -1.0f;
+
         if (sales <= 1000.0f)
         {
             Debug.Log("Pay: " + (200.0f + sales * 0.10f));
