3627a03214356adbce29d670116d6200669428df
Smiley98 <smileysofficialcinema@gmail.com>
2024-02-07 11:12:02 -0500

    Good buzz-fizz

--- Assets/Scripts/Loops.cs
+++ Assets/Scripts/Loops.cs
@@ -6,115 +6,20 @@ public class Loops : MonoBehaviour
 {
     void Start()
     {
-        int number = 1;
-
-        Debug.Log(number);
-        if (number % 3  == 0)       // if our number is divisible by 3:
+        // 1. Declare our counter (number)
+        // 2. Create our condition (<= 10 [less-than or equal-to 10])
+        // 3. Increment our counter (add 1 to number each time [number++])
+        for (int number = 1; number <= 10; number++)
         {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)       // if our number is divisible by 3:
-        {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)       // if our number is divisible by 3:
-        {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)       // if our number is divisible by 3:
-        {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)       // if our number is divisible by 3:
-        {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)       // if our number is divisible by 3:
-        {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)       // if our number is divisible by 3:
-        {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)       // if our number is divisible by 3:
-        {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)       // if our number is divisible by 3:
-        {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)       // if our number is divisible by 3:
-        {
-            Debug.Log("Buzz " + "(" + number + ")");
-        }
-        else if (number % 5 == 0)   // if our number is divisible by 5:
-        {
-            Debug.Log("Fizz " + "(" + number + ")");
+            Debug.Log(number);
+            if (number % 3 == 0)       // if our number is divisible by 3:
+            {
+                Debug.Log("Buzz " + "(" + number + ")");
+            }
+            else if (number % 5 == 0)   // if our number is divisible by 5:
+            {
+                Debug.Log("Fizz " + "(" + number + ")");
+            }
         }
     }
 }
