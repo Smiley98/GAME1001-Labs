ccc85886a4abd8bbd3b73e7e04996d4d4fca8325
Smiley98 <smileysofficialcinema@gmail.com>
2024-02-07 12:45:58 -0500

    Good fizz-buzz

--- Assets/Scripts/Loops.cs
+++ Assets/Scripts/Loops.cs
@@ -6,115 +6,20 @@ public class Loops : MonoBehaviour
 {
     void Start()
     {
-        int number = 1;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
+        // 1. Declare a counter variable (number = 1)
+        // 2. Set a condition (number <= 10) so we loop until the condition is false (number == 11)
+        // 3. Increment our counter (number = number + 1, or number++ for short)!
+        for (int number = 1; number <= 10; number++)
         {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
-        {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
-        {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
-        {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
-        {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
-        {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
-        {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
-        {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
-        {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
-        }
-        number++;
-
-        Debug.Log(number);
-        if (number % 3 == 0)
-        {
-            Debug.Log("Buzz");
-        }
-        if (number % 5 == 0)
-        {
-            Debug.Log("Fizz");
+            Debug.Log(number);
+            if (number % 3 == 0)
+            {
+                Debug.Log("Buzz");
+            }
+            if (number % 5 == 0)
+            {
+                Debug.Log("Fizz");
+            }
         }
     }
 }
