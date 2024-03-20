1aac7e4e029a2ee8213a47ba088061531004bdcb
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-20 10:40:00 -0400

    Added add function

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -9,11 +9,20 @@ public class Functions : MonoBehaviour
     void Start()
     {
         // We run a function by writing its name followed by parenthsis.
-        Debug.Log(Number());
+        Debug.Log(RandomNumber());
 
         // Just like variables, we can store function ouput for latter use!
-        int number = Number();
+        int number = RandomNumber();
         Debug.Log(number);
+
+        // Our add function needs 2 inputs & 1 output:
+        int a = RandomNumber();
+        int b = RandomNumber();
+        Debug.Log("Number 1: " + a);
+        Debug.Log("Number 2: " + b);
+        Debug.Log("Sum: " + Add(a, b));
+
+        // Task: Create a Sub, Mul, and Div function similar to the Add function
     }
 
     void Update()
@@ -23,9 +32,17 @@ public class Functions : MonoBehaviour
 
     // This function has no inputs (nothing in parenthesis),
     // but outputs a whole number (returns int])!
-    int Number()
+    int RandomNumber()
     {
         // Ouputs a random number between 0 and 9
         return Random.Range(0, 10);
+    }
+
+    //int Add(int a, int b) <-- "Function Signature" - tells us our function name, inputs & outputs
+    // {} <-- "Function Body" - contains the logic of the function (code that runs when we call our function)
+    // (Multiple words for "run" -- call, execute, invoke)
+    int Add(int a, int b)
+    {
+        return a + b;
     }
 }
