421c4bc1089885b57006b290f0974ef527590e4c
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-20 12:38:40 -0400

    Added decminal math functions (task 2)

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -10,22 +10,27 @@ public class Functions : MonoBehaviour
         // supplying their inputs within parentheis()
         // We can store the output of a function within a variable!
 
-        int a = RandomInteger();
-        int b = RandomInteger();
-        Debug.Log("Number 1: " + a);
-        Debug.Log("Number 2: " + b);
-        Debug.Log("Sum: " + Add(a, b));
-        Debug.Log("Difference: " + Sub(a, b));
-        Debug.Log("Product: " + Mul(a, b));
-        Debug.Log("Quotient: " + Div(a, b));
+        {
+            int a = RandomInteger();
+            int b = RandomInteger();
+            Debug.Log("Number 1: " + a);
+            Debug.Log("Number 2: " + b);
+            Debug.Log("Sum: " + Add(a, b));
+            Debug.Log("Difference: " + Sub(a, b));
+            Debug.Log("Product: " + Mul(a, b));
+            Debug.Log("Quotient: " + Div(a, b));
+        }
 
-        // Task: Create the same 4 functions but change them to use decimals instead
-        // Hint: All that needs to change are the data types in the signatures
-    }
-
-    void Update()
-    {
-        
+        {
+            float a = RandomDecimal();
+            float b = RandomDecimal();
+            Debug.Log("Number 1: " + a);
+            Debug.Log("Number 2: " + b);
+            Debug.Log("Sum: " + Add(a, b));
+            Debug.Log("Difference: " + Sub(a, b));
+            Debug.Log("Product: " + Mul(a, b));
+            Debug.Log("Quotient: " + Div(a, b));
+        }
     }
 
     // Outputs a random whole number between 0 and 9
@@ -34,6 +39,11 @@ public class Functions : MonoBehaviour
         return Random.Range(0, 10);
     }
 
+    // Outputs a random decimal number between 0 and 9
+    float RandomDecimal()
+    {
+        return Random.Range(0.0f, 10.0f);
+    }
 
     //int Add(int a, int b) <-- "function signature"
     // Function signatures contain all the information about a function.
@@ -66,6 +76,32 @@ public class Functions : MonoBehaviour
     }
 
     int Div(int a, int b)
+    {
+        return a / b;
+    }
+
+    //int number = 5;
+    //float number = 5.5f;
+    // Variables with the same name are compiler errors, but same-named functions are okay
+    // as long as their signatures are different!
+    // We call this "function overloading"
+
+    float Add(float a, float b)
+    {
+        return a + b;
+    }
+
+    float Sub(float a, float b)
+    {
+        return a - b;
+    }
+
+    float Mul(float a, float b)
+    {
+        return a * b;
+    }
+
+    float Div(float a, float b)
     {
         return a / b;
     }
